e2bec5fec909c94da11a9f7913e45886c41ac7da
Ang3l1t0 <1546@holbertonschool.com>
2020-08-17 16:37:05 -0500

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,6 @@
 # Prepare your web servers
 apt -y update
 apt install -y nginx
-sed -i '/server_name _/a location /redirect_me { rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwlsu4? permanent; }' /etc/nginx/sites-available/default
-sed -i "/server_name _/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default
-sed -i '/server_name _/a error_page 404 /404.html; location = /404.html {root /var/www/html/; internal; }' /etc/nginx/sites-available/default
-service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
@@ -13,4 +9,3 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
 service nginx restart
-exit 0
