643f48cda759ffa8940fa584666ff5a7251bb53b
Ang3l1t0 <1546@holbertonschool.com>
2020-09-01 16:06:20 -0500

    route /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+"""script that starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Hello Flask"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """Hello Flask"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
