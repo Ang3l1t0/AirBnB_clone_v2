6d4030ac23b404828d8b323a5d663e574bd89fb8
Ang3l1t0 <1546@holbertonschool.com>
2020-09-01 20:10:15 -0500

    it's a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Hello Flask"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """route hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """route params"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """route default params"""
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """route number"""
+    return n + " is a number"
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
