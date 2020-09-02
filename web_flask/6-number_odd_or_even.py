8a97fcb70beff833124aec37d9adaff97510dc42
Ang3l1t0 <1546@holbertonschool.com>
2020-09-02 12:41:45 -0500

    blanc space

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -41,6 +41,7 @@ def number_template(n):
     """render number_template"""
     return render_template('5-number.html', n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_template(n):
     """route number_odd_or_even"""
